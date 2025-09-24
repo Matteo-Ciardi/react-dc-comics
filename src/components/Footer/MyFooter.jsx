85ab91cd9a759b9788a516ba2fbfd7d2745374a3
MatteoCiardi <ciardimatteo99@gmail.com>
2025-09-24 22:42:06 +0200

    aggiunto container al footer top

--- src/components/Footer/MyFooter.jsx
+++ src/components/Footer/MyFooter.jsx
@@ -3,52 +3,54 @@ import './MyFooter.css'
 const MyFooter = () => {
     return (
         <footer>
-            <section className="footer-top">
-                <nav>
-                    <ul>
-                        <li><h3>DC COMICS</h3></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Collectibles</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">Fans</a></li>
-                        <li><a href="#">News</a></li>
-                    </ul>
-                    <ul>
-                        <li><h3>SHOP</h3></li>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
-                    </ul>
-                </nav>
-                <nav>
-                    <ul>
-                        <li><h3>DC</h3></li>
-                        <li><a href="#">Terms OF Use</a></li>
-                        <li><a href="#">Privacy Policy(New)</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Advertising</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">TalentWorkshops</a></li>
-                        <li><a href="#">CPSC Certificates</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Conact Us</a></li>
-                    </ul>
-                </nav>
-                <nav>
-                    <ul>
-                        <li><h3>SITES</h3></li>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
-                    </ul>
-                </nav>
+            <section className="footerBg">
+                <div className='footerContainer'>
+                    <nav>
+                        <ul>
+                            <li><h3>DC COMICS</h3></li>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Collectibles</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">Fans</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <ul>
+                            <li><h3>SHOP</h3></li>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </nav>
+                    <nav>
+                        <ul>
+                            <li><h3>DC</h3></li>
+                            <li><a href="#">Terms OF Use</a></li>
+                            <li><a href="#">Privacy Policy(New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">TalentWorkshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Conact Us</a></li>
+                        </ul>
+                    </nav>
+                    <nav>
+                        <ul>
+                            <li><h3>SITES</h3></li>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </nav>
+                </div>
 
             </section>
             <section className='footer-bottom'>
