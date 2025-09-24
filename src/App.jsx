6de239941aa994ba64a1e06ce902359d8e4d26ec
MatteoCiardi <ciardimatteo99@gmail.com>
2025-09-24 16:47:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import MyHeader from "./components/MyHeader"
+// import MyMain from "./components/MyMain"
+// import MyFooter from "./components/MyFooter"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MyHeader />
+      {/* <MyMain />
+      <MyFooter /> */}
+    </>
+  )
+}
+
+export default App
