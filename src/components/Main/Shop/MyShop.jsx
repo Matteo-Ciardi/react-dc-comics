89fbebec9fe61a30f2915741f96a3399b02ee776
MatteoCiardi <ciardimatteo99@gmail.com>
2025-09-24 22:46:29 +0200

    aggiunti link alle immagini shop

--- src/components/Main/Shop/MyShop.jsx
+++ src/components/Main/Shop/MyShop.jsx
@@ -7,27 +7,32 @@ const MyShop = () => {
             <div className='shopContainer'>
 
                 <figure className='itemPosition'>
-                    <img className='itemImage' src='../../src/assets/img/buy-comics-digital-comics.png'></img>
+                    <a href='#'><img className='itemImage' src='../../src/assets/img/buy-comics-digital-comics.png'></img>
+                    </a>
                     <figcaption className='itemName'><a href='#'>DIGITAL COMICS</a></figcaption>
                 </figure>
 
                 <figure className='itemPosition'>
-                    <img className='itemImage' src='../../src/assets/img/buy-comics-merchandise.png'></img>
+                    <a href='#'><img className='itemImage' src='../../src/assets/img/buy-comics-merchandise.png'></img>
+                    </a>
                     <figcaption className='itemName'><a href='#'>DC MERCHANDISE</a></figcaption>
                 </figure>
 
                 <figure className='itemPosition'>
-                    <img className='itemImage' src='../../src/assets/img/buy-comics-subscriptions.png'></img>
+                    <a href='#'><img className='itemImage' src='../../src/assets/img/buy-comics-subscriptions.png'></img>
+                    </a>
                     <figcaption className='itemName'><a href='#'>SUBSCRIPTION</a></figcaption>
                 </figure>
 
                 <figure className='itemPosition'>
-                    <img className='itemImage' src='../../src/assets/img/buy-comics-shop-locator.png'></img>
+                    <a href='#'><img className='itemImage' src='../../src/assets/img/buy-comics-shop-locator.png'></img>
+                    </a>
                     <figcaption className='itemName'><a href='#'>COMIC SHOP LOCATOR</a></figcaption>
                 </figure>
 
                 <figure className='itemPosition'>
-                    <img className='itemImage' src='../../src/assets/img/buy-dc-power-visa.svg'></img>
+                    <a href='#'><img className='itemImage' src='../../src/assets/img/buy-dc-power-visa.svg'></img>
+                    </a>
                     <figcaption className='itemName'><a href='#'>DC POWER VISA</a></figcaption>
                 </figure>
 
